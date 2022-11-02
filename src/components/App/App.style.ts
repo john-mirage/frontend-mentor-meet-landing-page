70d7a419807ecee7665958f2274c482c253008b7
john-mirage <28307090+john-mirage@users.noreply.github.com>
2022-11-02 16:51:22 +0100

    fix: fix page top padding

--- src/components/App/App.style.ts
+++ src/components/App/App.style.ts
@@ -3,7 +3,10 @@ import BaseHeader from "@components/Header";
 import BaseHero from "@components/Hero";
 
 export const Container = styled("div", {
-  paddingTop: "$48",
+  "paddingTop": "$48",
+  "@lg": {
+    paddingTop: "$80",
+  },
 });
 
 export const Header = styled(BaseHeader, {
